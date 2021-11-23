0dea891394120a703b273d063f155ba3dce6fddd
Tamil <selva221724@gmail.com>
2021-11-24 00:58:58 +0530

    updated

--- dummylog.py
+++ dummylog.py
@@ -1,8 +1,8 @@
 """
-Lumache - Python library for cooks and food lovers.
+dummylog - Python library for cooks and food lovers.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 
 
 class InvalidKindError(Exception):
