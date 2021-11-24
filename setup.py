54b047facb803c02bab9e31b26c491d0ea78fab5
Tamil <selva221724@gmail.com>
2021-11-25 01:34:07 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -16,11 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),  # List of all python modules to be installed
     classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Framework :: IPython",
         "Operating System :: Microsoft :: Windows :: Windows 10",
